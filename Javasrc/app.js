413930e92fc4c15b279b6f30637ca5ab16fb9945
maris <moris.gravitis@gmail.com>
2024-03-27 15:18:17 +0200

    dsfg

---
+++ Javasrc/app.js
@@ -0,0 +1,43 @@
+let num = 42 // number
+let firstName ='Maris' // string
+const isProgrammer = true  // boolean
+// можно делать 
+/*let $ = 'test'
+let $num = 42
+let num$ =42
+let_ = 42
+let _num = 12
+let num_ = 12
+let first_name = 'maris'*/
+//firstName = 'Max'
+/* запрещенное 
+let 42num = '11'
+let my-num = 1
+let const*/
+//alert(firstName )
+//console.log('test',num , isProgrammer)
+/*console.log(num+10)
+console.log(num-10)
+console.log(num*10)
+console.log(num/10)
+console.log(num)*/
+
+/*let num2 = num +10
+console.log(num,num2)
+num = num2-num
+console.log(num,num2)*/
+
+//const FullName = firstName + ' mor'
+//console.log(FullName)
+const resultElement = document.getElementById('result')
+console.log(resultElement)
+
+
+
+
+
+
+
+
+
+
